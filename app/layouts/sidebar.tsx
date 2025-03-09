b8aa3a3964ca5c8340564bb2258bdcb0642096e1
s-javier <javiersalamank@gmail.com>
2025-03-09 18:45:18 -0300

    Wip

--- app/layouts/sidebar.tsx
+++ app/layouts/sidebar.tsx
@@ -1,14 +1,22 @@
-import { Form, Link, Outlet } from 'react-router'
+import { Form, Link, NavLink, Outlet, useNavigation } from 'react-router'
 import { getContacts } from '../data'
 import type { Route } from './+types/sidebar'
 
-export async function clientLoader() {
-  const contacts = await getContacts()
+// export async function clientLoader() {
+//   const contacts = await getContacts()
+//   return { contacts }
+// }
+
+export async function loader({ request }: Route.LoaderArgs) {
+  const url = new URL(request.url)
+  const q = url.searchParams.get('q')
+  const contacts = await getContacts(q)
   return { contacts }
 }
 
 export default function SidebarLayout({ loaderData }: Route.ComponentProps) {
   const { contacts } = loaderData
+  const navigation = useNavigation()
 
   return (
     <>
@@ -36,7 +44,12 @@ export default function SidebarLayout({ loaderData }: Route.ComponentProps) {
             <ul>
               {contacts.map((contact) => (
                 <li key={contact.id}>
-                  <Link to={`contacts/${contact.id}`}>
+                  <NavLink
+                    className={({ isActive, isPending }) =>
+                      isActive ? 'active' : isPending ? 'pending' : ''
+                    }
+                    to={`contacts/${contact.id}`}
+                  >
                     {contact.first || contact.last ? (
                       <>
                         {contact.first} {contact.last}
@@ -45,7 +58,7 @@ export default function SidebarLayout({ loaderData }: Route.ComponentProps) {
                       <i>No Name</i>
                     )}
                     {contact.favorite ? <span>★</span> : null}
-                  </Link>
+                  </NavLink>
                 </li>
               ))}
             </ul>
@@ -56,7 +69,7 @@ export default function SidebarLayout({ loaderData }: Route.ComponentProps) {
           )}
         </nav>
       </div>
-      <div id="detail">
+      <div id="detail" className={navigation.state === 'loading' ? 'loading' : ''}>
         <Outlet />
       </div>
     </>
--- app/root.tsx
+++ app/root.tsx
@@ -1,13 +1,13 @@
-import { Form, Link, Outlet, Scripts, ScrollRestoration, isRouteErrorResponse } from 'react-router'
+import { Outlet, Scripts, ScrollRestoration, isRouteErrorResponse, redirect } from 'react-router'
 import type { Route } from './+types/root'
 
 import appStylesHref from './app.css?url'
-import { getContacts } from './data'
+import { createEmptyContact } from './data'
 
-// export async function clientLoader() {
-//   const contacts = await getContacts()
-//   return { contacts }
-// }
+export async function action() {
+  const contact = await createEmptyContact()
+  return redirect(`/contacts/${contact.id}/edit`)
+}
 
 export function HydrateFallback() {
   return (
